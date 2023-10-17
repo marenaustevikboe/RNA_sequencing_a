1603448e8fbe4e0e974eb1600886d8b7635d3f44
marenaustevikboe <xiq019@uib.no>
2023-10-17 12:39:50 +0200

    saving

--- Scripts/Tibble.R
+++ Scripts/Tibble.R
@@ -9,17 +9,21 @@ glimpse(RNAseq)
 library(dplyr)
 
 # Filter rows with p-value less than 0.05 (significant)
-significant_rows <- RNAseq %>%
-  filter("P-value" < 0.05)
+significant_rows <- filter(RNAseq, `P-value` < 0.05)
 glimpse(significant_rows)
 
 #Filter great log2FC values
 tidyData <- significant_rows %>%
-  filter("Log2(FC)"<-1) %>%
-  filter("Log2(FC)">1)
+  filter(`log2(FC)`<-1) %>%
+  filter(`log2(FC)`>1)
 
 #Choosing colums I want to work with
 extracted_columns <- tidyData[, c("GeneID", "log2(FC)")]
 glimpse(extracted_columns)
 
 library(ggplot2)
+
+
+
+
+
